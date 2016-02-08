00422b1674245e8204467d33ad677416c4d02577
Anders Lundsgård <anders@utvecklare.eu>
2016-02-08 22:28:32 +0100

    PS1 for starting MongoDB as Windows Service

---
+++ Start-MongoDB.ps1
@@ -0,0 +1,5 @@
+﻿New-Item 'c:\mongodb' -Type Directory -Force
+
+Start-Process 'C:\Program Files\MongoDB\Server\3.2\bin\mongod.exe' -ArgumentList '--dbpath=c:\mongodb --logpath=c:\mongodb\log.txt --install' -Wait
+
+Start-Service MongoDB
