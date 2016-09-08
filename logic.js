c593dcbabb47e53f0ff4da10ae313253fcdb46c2
Anders Lundsgård <anders@utvecklare.eu>
2016-09-08 20:52:50 +0200

    Refactor logic to separate file

---
+++ logic.js
@@ -0,0 +1,24 @@
+
+exports.sunrise = function(lat, lon, date){
+
+    var suncalc = require('suncalc');
+
+    //var lat = req.query.lat;
+    //var lon = req.query.lon;
+    //var date = new Date(req.query.date);
+    var suntimes = suncalc.getTimes(date, lat, lon);
+    var moontimes = suncalc.getMoonTimes(date, lat, lon);
+    var moonillumination = suncalc.getMoonIllumination(date);
+
+    // merge sun and moon results
+    var alltimes = suntimes;
+
+    alltimes["moonrise"] = moontimes["rise"];
+    alltimes["moonset"] = moontimes["set"];
+
+    alltimes["moonfraction"] = moonillumination["fraction"];
+    alltimes["moonphase"] = moonillumination["phase"];
+    alltimes["moonangle"] = moonillumination["angle"];
+
+    return alltimes;
+}
--- server.js
+++ server.js
@@ -1,26 +1,12 @@
+var logic = require('./logic.js');
+
 var express = require('express');
 var app = express();
-var suncalc = require('suncalc');
-var mongoose = require('mongoose');
 
 // Sample call: http://localhost:3000/?lat=59.2&lon=17.4&date=2016-01-14
 app.get('*', function(req, res) {
-    var lat = req.query.lat;
-    var lon = req.query.lon;
-    var date = new Date(req.query.date);
-    var suntimes = suncalc.getTimes(date, lat, lon);
-    var moontimes = suncalc.getMoonTimes(date, lat, lon);
-    var moonillumination = suncalc.getMoonIllumination(date);
 
-    // merge sun and moon results
-    var alltimes = suntimes;
-
-    alltimes["moonrise"] = moontimes["rise"];
-    alltimes["moonset"] = moontimes["set"];
-
-    alltimes["moonfraction"] = moonillumination["fraction"];
-    alltimes["moonphase"] = moonillumination["phase"];
-    alltimes["moonangle"] = moonillumination["angle"];
+    var alltimes = logic.sunrise(req.query.lat, req.query.lon, new Date(req.query.date));
 
     res.json(alltimes);
 });
