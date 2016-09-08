78762dda2b3ef7c2fdd33c62a774917f493b0200
Anders Lundsgård <anders@utvecklare.eu>
2016-09-08 20:22:58 +0200

    Script to deploy to AWS Lambda
    Security credentials needs to be configured by AWS CLI in advanced 'aws
    configure'.

---
+++ DeployToAwsLambda.ps1
@@ -0,0 +1,11 @@
+﻿## Pre-Req: 
+# aws configure
+# with access and security keys
+
+# Create Deploy package
+Compress-Archive ./** -Update -DestinationPath '.\sunrise.zip'
+
+Start-Sleep -Seconds 5
+
+# Deploy to Lambda latest version
+aws lambda update-function-code --function-name 'sunrise' --zip-file 'fileb://.\sunrise.zip' --publish 
