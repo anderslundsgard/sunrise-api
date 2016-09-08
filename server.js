314d5019e69689999fb13f86d9c259826aa2631a
Anders Lundsgård <anders@utvecklare.eu>
2016-09-08 20:34:36 +0200

    Revert "Cleanup..."
    This reverts commit e193f670d7de829df20051c6c3e997b2469501d7.

--- server.js
+++ server.js
@@ -1,47 +1,41 @@
-exports.handler = function(event, context, callback) {
+var express = require('express');
+var app = express();
+var suncalc = require('suncalc');
+var mongoose = require('mongoose');
 
-    console.log('Running sunrise-api...');
-
-    var express = require('express');
-    var app = express();
-    var suncalc = require('suncalc');
-    //var mongoose = require('mongoose');
-
-    //mongoose.connect('mongodb://localhost/sunrisedb');
-    //var db = mongoose.connection;
-    //db.on('error', console.error.bind(console, 'connection error...'));
-    //db.once('open', function callback() {
-    //    console.log('multivision db opened');
-    //});
-
-    module.exports = app;
+mongoose.connect('mongodb://localhost/sunrisedb');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error...'));
+db.once('open', function callback() {
+    console.log('multivision db opened');
+});
 
 // Sample call: http://localhost:3000/?lat=59.2&lon=17.4&date=2016-01-14
-    app.get('*', function (req, res) {
-        var lat = req.query.lat;
-        var lon = req.query.lon;
-        var date = new Date(req.query.date);
-        var suntimes = suncalc.getTimes(date, lat, lon);
-        var moontimes = suncalc.getMoonTimes(date, lat, lon);
-        var moonillumination = suncalc.getMoonIllumination(date);
+app.get('*', function(req, res) {
+    var lat = req.query.lat;
+    var lon = req.query.lon;
+    var date = new Date(req.query.date);
+    var suntimes = suncalc.getTimes(date, lat, lon);
+    var moontimes = suncalc.getMoonTimes(date, lat, lon);
+    var moonillumination = suncalc.getMoonIllumination(date);
 
-        // merge sun and moon results
-        var alltimes = suntimes;
+    // merge sun and moon results
+    var alltimes = suntimes;
 
-        alltimes["moonrise"] = moontimes["rise"];
-        alltimes["moonset"] = moontimes["set"];
+    alltimes["moonrise"] = moontimes["rise"];
+    alltimes["moonset"] = moontimes["set"];
 
-        alltimes["moonfraction"] = moonillumination["fraction"];
-        alltimes["moonphase"] = moonillumination["phase"];
-        alltimes["moonangle"] = moonillumination["angle"];
+    alltimes["moonfraction"] = moonillumination["fraction"];
+    alltimes["moonphase"] = moonillumination["phase"];
+    alltimes["moonangle"] = moonillumination["angle"];
 
-        res.json(alltimes);
-    });
+    res.json(alltimes);
+});
 
-    var port = 3000;
-    app.listen(port);
+var port = 3000;
+app.listen(port);
+console.log('Listen on port: ' + port + '...');
 
-}
 // Test calls
 // http://localhost:3000/?lat=59.22&lon=17.44&date=2012-06-23
 // Compare with api.met.no
