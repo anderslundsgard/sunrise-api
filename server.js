2ea9d72a9aa7188071f93424a20480bf56d075bc
Anders Lundsgård <anders@utvecklare.eu>
2016-09-08 20:36:41 +0200

    Clean away Mongo stuff

--- server.js
+++ server.js
@@ -3,13 +3,6 @@ var app = express();
 var suncalc = require('suncalc');
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/sunrisedb');
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error...'));
-db.once('open', function callback() {
-    console.log('multivision db opened');
-});
-
 // Sample call: http://localhost:3000/?lat=59.2&lon=17.4&date=2016-01-14
 app.get('*', function(req, res) {
     var lat = req.query.lat;
